59b6932dea30d61c8776ed6b291ded45c3e25b4c
NickTasche <112028277+NickTasche@users.noreply.github.com>
2022-10-06 15:12:20 -0700

    Update index.js

--- Woodland-Test/index.js
+++ Woodland-Test/index.js
@@ -45,11 +45,11 @@ app.get('/ab*cd', function(req, res) {
    res.send('Page Pattern Match');
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+//var server = app.listen(8081, function () {
+  // var host = server.address().address
+ //  var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
+ //  console.log("Example app listening at http://%s:%s", host, port)
 
 
 
